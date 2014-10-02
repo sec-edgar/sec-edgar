45e1fc0b69cdaf41c489f34424bace2eaf52ee92
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-10-02 12:40:08 +0530

    add test

--- SECEdgar/crawler.py
+++ SECEdgar/crawler.py
@@ -11,7 +11,7 @@ class SecCralwer():
 	def __init__(self):
 		self.hello = "Welcome to Sec Cralwer!"
 
-	def filing_10Q(self, companyCode, cik, priorto):
+	def make_directory(self, companyCode, cik, priorto, filing_type):
 		# Making the directory to save comapny filings
 		if not os.path.exists("SEC-Edgar-data/"):
 			os.makedirs("SEC-Edgar-data/")
@@ -19,11 +19,28 @@ class SecCralwer():
 			os.makedirs("SEC-Edgar-data/"+str(companyCode))
 		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)):
 			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-Q")):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-Q"))
+		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("filing_type")):
+			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("filing_type"))
+	
+	def save_in_directory(self, companyCode, cik, priorto, docList, docNameList, filing_type):
+		# Save every text document into its respective folder
+		for j in range(len(docList)):
+			base_url = docList[j]
+			r = requests.get(base_url)
+			data = r.text
+			path = "SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str(filing_type)+"/"+str(docNameList[j])
+			filename = open(path,"a")
+			filename.write(data.encode('ascii', 'ignore'))	
+
+
+	def filing_10Q(self, companyCode, cik, priorto, count):
+		try:
+			self.make_directory(companyCode,cik, priorto, '10-Q')
+		except:
+			print "Not able to create directory"
 		
 		#generate the url to crawl 
-		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=10-Q&dateb="+str(priorto)+"&owner=exclude&output=xml&count=100"	
+		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=10-Q&dateb="+str(priorto)+"&owner=exclude&output=xml&count="+str(count)	
 		print ("started 10-Q"+str(companyCode))
 		r = requests.get(base_url)
 		data = r.text
@@ -37,7 +54,10 @@ class SecCralwer():
 				URL+="l"
 	    		linkList.append(URL)
 		linkListFinal = linkList
-		print "List Completed"
+		
+		print ("Number of files to download %s", len(linkListFinal))
+		print ("Starting download....")
+
 		docList = [] # List of URL to the text documents
 		docNameList = [] # List of document names
 
@@ -48,28 +68,23 @@ class SecCralwer():
 			docname = txtdoc.split("/")[len(txtdoc.split("/"))-1]
 			docList.append(txtdoc)
 			docNameList.append(docname)
-		# Save every text document into its respective folder
-		for j in range(len(docList)):
-			base_url = docList[j]
-			r = requests.get(base_url)
-			data = r.text
-			path = "SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-Q")+"/"+str(docNameList[j])
-			filename = open(path,"a")
-			filename.write(data.encode('ascii', 'ignore'))	
+		
+		try:
+			self.save_in_directory(companyCode, cik, priorto, docList, docNameList, '10-Q')
+		except:
+			print "Not able to save the file :( "
 
-	def filing_10K(self, companyCode, cik, priorto):
-		#Making the directory to save comapny filings
-		if not os.path.exists("SEC-Edgar-data/"):
-			os.makedirs("SEC-Edgar-data/")
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-K")):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-K"))
+		print "Successfully downloaded all the files"
+
+
+	def filing_10K(self, companyCode, cik, priorto, count):
+		try:
+			self.make_directory(companyCode,cik, priorto, '10-K')
+		except:
+			print "Not able to create directory"
 		
 		#generate the url to crawl 
-		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=10-K&dateb="+str(priorto)+"&owner=exclude&output=xml&count=100"	
+		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=10-K&dateb="+str(priorto)+"&owner=exclude&output=xml&count="+str(count)	
 		print ("started !0-K"+str(companyCode))
 		r = requests.get(base_url)
 		data = r.text
@@ -83,7 +98,10 @@ class SecCralwer():
 				URL+="l"
 	    		linkList.append(URL)
 		linkListFinal = linkList
-		print "List Completed"
+		
+		print ("Number of files to download %s", len(linkListFinal))
+		print ("Starting download....")
+
 		docList = [] # List of URL to the text documents
 		docNameList = [] # List of document names
 
@@ -93,28 +111,22 @@ class SecCralwer():
 			docname = txtdoc.split("/")[len(txtdoc.split("/"))-1]
 			docList.append(txtdoc)
 			docNameList.append(docname)
-		# Save every text document into its respective folder
-		for j in range(len(docList)):
-			base_url = docList[j]
-			r = requests.get(base_url)
-			data = r.text
-			path = "SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("10-K")+"/"+str(docNameList[j])
-			filename = open(path,"a")
-			filename.write(data.encode('ascii', 'ignore'))	
+		
+		try:
+			self.save_in_directory(companyCode, cik, priorto, docList, docNameList, '10-K')
+		except:
+			print "Not able to save the file :( "
 
-	def filing_8K(self, companyCode, cik, priorto):
-		#Making the directory to save comapny filings
-		if not os.path.exists("SEC-Edgar-data/"):
-			os.makedirs("SEC-Edgar-data/")
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("8-K")):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("8-K"))
+		print "Successfully downloaded all the files"
+
+	def filing_8K(self, companyCode, cik, priorto, count):
+		try:
+			self.make_directory(companyCode,cik, priorto, '8-K')
+		except:
+			print "Not able to create directory"
 		
 		#generate the url to crawl 
-		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=8-K&dateb="+str(priorto)+"&owner=exclude&output=xml&count=100"	
+		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=8-K&dateb="+str(priorto)+"&owner=exclude&output=xml&count="+str(count)	
 		print ("started 8-K" + str(companyCode))
 		r = requests.get(base_url)
 		data = r.text
@@ -128,7 +140,10 @@ class SecCralwer():
 				URL+="l"
 	    		linkList.append(URL)
 		linkListFinal = linkList
-		print "List Completed"
+		
+		print ("Number of files to download %s", len(linkListFinal))
+		print ("Starting download....")
+
 		docList = [] # List of URL to the text documents
 		docNameList = [] # List of document names
 
@@ -138,28 +153,22 @@ class SecCralwer():
 			docname = txtdoc.split("/")[len(txtdoc.split("/"))-1]
 			docList.append(txtdoc)
 			docNameList.append(docname)
-		# Save every text document into its respective folder
-		for j in range(len(docList)):
-			base_url = docList[j]
-			r = requests.get(base_url)
-			data = r.text
-			path = "SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("8-K")+"/"+str(docNameList[j])
-			filename = open(path,"a")
-			filename.write(data.encode('ascii', 'ignore'))	
+		
+		try:
+			self.save_in_directory(companyCode, cik, priorto, docList, docNameList, '8-K')
+		except:
+			print "Not able to save the file :( "
 
-	def filing_13F(self, companyCode, cik, priorto):
-		# Making the directory to save comapny filings
-		if not os.path.exists("SEC-Edgar-data/"):
-			os.makedirs("SEC-Edgar-data/")
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik))
-		if not os.path.exists("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("13-F")):
-			os.makedirs("SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("13-F"))
+		print "Successfully downloaded all the files"
+
+	def filing_13F(self, companyCode, cik, priorto, count):
+		try:
+			self.make_directory(companyCode,cik, priorto, '13-F')
+		except:
+			print "Not able to create directory"
 		
 		#generate the url to crawl 
-		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=13F&dateb="+str(priorto)+"&owner=exclude&output=xml&count=100"	
+		base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK="+str(cik)+"&type=13F&dateb="+str(priorto)+"&owner=exclude&output=xml&count="+str(count)	
 		print ("started 10-Q"+str(companyCode))
 		r = requests.get(base_url)
 		data = r.text
@@ -173,7 +182,10 @@ class SecCralwer():
 				URL+="l"
 	    		linkList.append(URL)
 		linkListFinal = linkList
-		print "List Completed"
+		
+		print ("Number of files to download %s", len(linkListFinal))
+		print ("Starting download....")
+
 		docList = [] # List of URL to the text documents
 		docNameList = [] # List of document names
 
@@ -184,11 +196,11 @@ class SecCralwer():
 			docname = txtdoc.split("/")[len(txtdoc.split("/"))-1]
 			docList.append(txtdoc)
 			docNameList.append(docname)
-		# Save every text document into its respective folder
-		for j in range(len(docList)):
-			base_url = docList[j]
-			r = requests.get(base_url)
-			data = r.text
-			path = "SEC-Edgar-data/"+str(companyCode)+"/"+str(cik)+"/"+str("13-F")+"/"+str(docNameList[j])
-			filename = open(path,"a")
-			filename.write(data.encode('ascii', 'ignore'))	
+
+		try:
+			self.save_in_directory(companyCode, cik, priorto, docList, docNameList, '13-F')
+		except:
+			print "Not able to save the file :( "
+
+		print "Successfully downloaded all the files"
+
--- SECEdgar/test.py
+++ SECEdgar/test.py
@@ -1,12 +1,17 @@
 import time
-from sec import Filing_10Q, Filing_10K, Filing_8K
+from crawler import SecCrawler
+
 
 def test():
 	t1 = time.time()
 	# file containig company name and corresponding cik codes
+	seccrawler = SecCrawler()
+
 	companyCodeList = list()    # company code list 
-	cikList = list()	    # cik code list
+	cikList = list()	        # cik code list
 	dateList = list()           # pror date list
+	countList = list()
+	
 	try:
 		crs = open("file.txt", "r")
 	except:
@@ -17,12 +22,13 @@ def test():
 	     	companyCodeList.append(columns[0])
 		cikList.append(columns[1])
 		dateList.append(columns[2])
+		countList.append(columns[3])
 
 	del cikList[0]; del companyCodeList[0]; del dateList[0]
 	for i in range(len(cikList)):
-		Filing_10Q(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]))
-		Filing_10K(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]))
-		Filing_8K(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]))
+		seccrawler.filing_10Q(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]), str(countList[i]))
+		seccrawler.filing_10K(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]), str(countList[i]))
+		seccrawler.filing_8K(str(companyCodeList[i]), str(cikList[i]), str(dateList[i]), str(countList[i]))
 	
 	t2 = time.time()
 	print "Total Time taken: ",
