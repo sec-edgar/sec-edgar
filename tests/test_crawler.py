cb1426f62f181192f86141f63ffb9a19175dce35
jackmoody11 <jackmoody@unc.edu>
2019-04-24 09:43:32 +0200

    Rename tests and add test for form 4

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -3,21 +3,26 @@ import pytest
 
 class TestCrawler(object):
     @pytest.mark.xfail(raises=TypeError)
-    def test_10Q_positional_requirements():
+    def test_10Q_requires_args():
         return crawler.filing_10Q()
 
     @pytest.mark.xfail(raises=TypeError)
-    def test_10K_positional_requirements():
+    def test_10K_requires_args():
         return crawler.filing_10K()
 
     @pytest.mark.xfail(raises=TypeError)
-    def test_SD_positional_requirements():
+    def test_SD_requires_args():
         return crawler.filing_SD()
 
     @pytest.mark.xfail(raises=TypeError)
-    def test_8K_positional_requirements():
+    def test_8K_requires_args():
         return crawler.filing_8K()
 
     @pytest.mark.xfail(raises=TypeError)
-    def test_13F_positional_requirements():
+    def test_13F_requires_args():
         return crawler.filing_13F()
+    
+    @pytest.mark.xfail(raises=TypeError)
+    def test_4_requires_args():
+        return crawler.filing_4()
+
