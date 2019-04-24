0a34cf8d0a74e2af46f8b8e8e1ae1a32f47585a3
jackmoody11 <jackmoody@unc.edu>
2019-04-24 14:35:45 +0200

    Modularize tests

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,28 +1,7 @@
-import pytest
+# Tests internal functions from SecCrawler
+import os
 
 
-class TestCrawler(object):
-    @pytest.mark.xfail(raises=TypeError)
-    def test_10Q_requires_args():
-        return crawler.filing_10Q()
-
-    @pytest.mark.xfail(raises=TypeError)
-    def test_10K_requires_args():
-        return crawler.filing_10K()
-
-    @pytest.mark.xfail(raises=TypeError)
-    def test_SD_requires_args():
-        return crawler.filing_SD()
-
-    @pytest.mark.xfail(raises=TypeError)
-    def test_8K_requires_args():
-        return crawler.filing_8K()
-
-    @pytest.mark.xfail(raises=TypeError)
-    def test_13F_requires_args():
-        return crawler.filing_13F()
-    
-    @pytest.mark.xfail(raises=TypeError)
-    def test_4_requires_args():
-        return crawler.filing_4()
-
+def test_fetch_report(crawler, valid_fetch_report_args):
+    crawler._fetch_report(**valid_fetch_report_args)
+    assert os.path.isdir(crawler.data_path)
