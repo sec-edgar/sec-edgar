0464132724be7d30341efb3e2b27284fdfb8c1d2
jackmoody11 <jackmoody@unc.edu>
2019-05-08 15:49:27 -0400

    Add more detailed content to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
 # -*- coding:utf-8 -*-
 import sys
-
+import os
+import re
+import codecs
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 SUPPORTED_VERSIONS = ['2.7', '3.5', '3.6', 'PyPy', ]
+here = os.path.abspath(os.path.dirname(__file__))
 
-if sys.version_info <= (2, 4):
-    version = '.'.join([str(x) for x in sys.version_info[:3]])
-    suppored_versions = ', '.join(SUPPORTED_VERSIONS)
-    print('Version {version} is not supported. Supported versions are {supported_versions}'
-          .format(version=version, supported_versions=SUPPORTED_VERSIONS)
-          )
-    sys.exit(1)
+
+def find_version(*file_paths):
+    """Read the version number from a source file.
+    Why read it, and not import?
+    see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+    """
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 def parse_requirements(filename):
@@ -22,10 +36,15 @@ def parse_requirements(filename):
         return required
 
 
+# Get the long description from the relevant file
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='SECEdgar',
-    version='0.1.4',
-    packages=find_packages(),
+    version=find_version('SECEdgar', '__init__.py'),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     package_dir={'SECEdgar': 'SECEdgar'},
     url='https://github.com/rahulrrixe/SEC-Edgar',
     license='Apache License (2.0)',
@@ -35,6 +54,8 @@ setup(
                  filings. It provides an interface to extract the filing from 
                  the SEC.gov site. You might find it most useful for tasks 
                  involving automated data collection of filings from SEC.gov""",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     entry_points='''
             [console_scripts]
             ''',
@@ -53,5 +74,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy']
+        'Programming Language :: Python :: Implementation :: PyPy'],
+    package_data={
+        'SECEdgar': [],
+    }
 )
