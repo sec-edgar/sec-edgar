747e0f91439aba77d3a2781f74640f240dd592a1
jackmoody11 <jackmoody@unc.edu>
2019-05-17 12:30:14 -0400

    Add deprecation warning

--- SECEdgar/crawler.py
+++ SECEdgar/crawler.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 import errno
 from SECEdgar.utils.exceptions import EDGARQueryError, CIKError
 from SECEdgar.utils import _sanitize_date
+import warnings
 
 DEFAULT_DATA_PATH = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', 'SEC-Edgar-Data'))
@@ -16,6 +17,9 @@ DEFAULT_DATA_PATH = os.path.abspath(os.path.join(
 
 class SecCrawler(object):
     """Main crawler object for SEC filings. """
+    warnings.warn("The SecCrawler class will be deprecated "
+                  "in favor of the classes in "
+                  "SECEdgar.filings beginning in v0.2.0.")
 
     def __init__(self, data_path=DEFAULT_DATA_PATH):
         self.data_path = data_path
