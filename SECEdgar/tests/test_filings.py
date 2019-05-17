9aee125fb51193666baabcee8589b4525a6955b7
jackmoody11 <jackmoody@unc.edu>
2019-05-17 12:31:53 -0400

    Update tests for new file structure

--- SECEdgar/tests/test_filings.py
+++ SECEdgar/tests/test_filings.py
@@ -1,5 +1,8 @@
 # Tests if filings are correctly received from EDGAR
 import pytest
+from SECEdgar.filings import Filing10Q
+import datetime
+import requests
 
 
 def test_10Q_requires_args(crawler):
@@ -30,3 +33,26 @@ def test_13F_requires_args(crawler):
 def test_4_requires_args(crawler):
     with pytest.raises(TypeError):
         return crawler.filing_4()
+
+
+class TestFilings(object):
+    def test_count_returns_exact(self, filing_10Q):
+        if not len(filing_10Q._get_urls()) == 3:
+            raise AssertionError("Count should return exact number of filings.")
+
+    def test_date_is_sanitized(self, filing_10Q):
+        date = datetime.datetime(2015, 1, 1)
+        filing_10Q.dateb = date
+        if not filing_10Q.dateb == '20150101':
+            raise AssertionError("The dateb param was not correctly sanitized.")
+
+    def test_txt_urls(self, filing_10Q):
+        r = requests.get(filing_10Q._get_urls()[0])
+        print(r.text)
+        if not r.text:
+            raise AssertionError("Text file returned as empty.")
+
+    def test_filing_type_immutable(self, filing_10Q):
+        """The filing_type property should be immutable. """
+        with pytest.raises(AttributeError):
+            filing_10Q.filing_type = "10-K"
