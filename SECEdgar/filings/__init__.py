2ed3f62bc9ea2779d88b0438de1390177f17d598
jackmoody11 <jackmoody@unc.edu>
2019-05-17 14:57:05 -0400

    Add noqa for flake8

--- SECEdgar/filings/__init__.py
+++ SECEdgar/filings/__init__.py
@@ -1,3 +1,3 @@
-from SECEdgar.base import _EDGARBase
-from SECEdgar.filings.base import (Filing10K, Filing10Q, Filing13F,
+from SECEdgar.base import _EDGARBase  # noqa:F401
+from SECEdgar.filings.base import (Filing10K, Filing10Q, Filing13F,  # noqa:F401
                                    Filing4, Filing8K, FilingSD)
