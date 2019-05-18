286c26c5cfc601ed21e7f44678ee9dd9fcee1b81
jackmoody11 <jackmoody@unc.edu>
2019-05-18 18:54:20 -0400

    Change Travis testing to setup as would be done from PyPi

--- setup.py
+++ setup.py
@@ -29,16 +29,17 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-def parse_requirements(filename):
-
-    with open(filename) as f:
-        required = f.read().splitlines()
-        return required
+def parse_requirements(*files):
+    required = []
+    for file in files:
+        with open(file) as f:
+            required.append(f.read().splitlines())
+    return required
 
 
 # Get the long description from the relevant file
 with codecs.open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 
 setup(
@@ -47,13 +48,14 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     package_dir={'SECEdgar': 'SECEdgar'},
     url='https://github.com/rahulrrixe/SEC-Edgar',
+    download_url='https://github.com/rahulrrixe/SEC-Edgar/releases',
     license='Apache License (2.0)',
     author='Rahul Ranjan',
     author_email='rahul.rrixe@gmail.com',
     description="""SEC-Edgar implements a basic crawler for downloading 
                  filings from the SEC Edgar database. It is most useful 
                  for automatically collecting public filings from the SEC.""",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
     entry_points='''
             [console_scripts]
@@ -61,9 +63,14 @@ setup(
     cmdclass={
         'install': install,
     },
+
+    # List run-time dependencies here. These will be installed by pip when your
+    # project is installed.
     install_requires=parse_requirements('requirements.txt'),
     keywords=['SEC', 'Edgar', 'Crawler', 'filings'],
-    tests_require=parse_requirements('requirements.txt'),
+    test_suite='pytest',
+    setup_requires=['pytest-runner'],
+    tests_require=parse_requirements('requirements.txt', 'requirements-dev.txt'),
     classifiers=[
         'Environment :: Console',
         'License :: OSI Approved :: Apache Software License',
@@ -75,6 +82,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    # If there are data files included in your packages that need to be
+    # installed, specify them here. If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
     package_data={
         'SECEdgar': [],
     }
