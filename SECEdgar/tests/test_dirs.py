b3b16908cb2b68701b3c3f5ef22ab2903ffe5a11
jackmoody11 <jackmoody@unc.edu>
2019-05-20 16:27:08 -0400

    Get rid of shutil remove directories for Travis

--- SECEdgar/tests/test_dirs.py
+++ SECEdgar/tests/test_dirs.py
@@ -1,9 +1,7 @@
 # Tests if directories correctly built for files
 import os
-import shutil
 
 
 def test_if_default_data_path_built(crawler, valid_make_dir_args):
     crawler._make_directory(**valid_make_dir_args)
     assert os.path.isdir(crawler.data_path)
-    shutil.rmtree(crawler.data_path)
