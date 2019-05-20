59f285a3a6e44b2c28a956b3b54152625cfe81d0
jackmoody11 <jackmoody@unc.edu>
2019-05-20 16:01:54 -0400

    Change single quotes to double

--- SECEdgar/tests/conftest.py
+++ SECEdgar/tests/conftest.py
@@ -1,31 +1,31 @@
 import pytest
 from SECEdgar.crawler import SecCrawler
-from SECEdgar.filings import Filings
+from SECEdgar.filings import Filing
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def valid_make_dir_args():
-    return {'company_code': 'AAPL', 'cik': '0000320193',
-            'priorto': '20010101', 'filing_type': '10-Q'}
+    return {"company_code": "AAPL", "cik": "0000320193",
+            "priorto": "20010101", "filing_type": "10-Q"}
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def valid_filing_data():
-    return {'company_code': 'AAPL', 'cik': '0000320193',
-            'priorto': '20010101', 'count': 1}
+    return {"company_code": "AAPL", "cik": "0000320193",
+            "priorto": "20010101", "count": 1}
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def valid_fetch_report_args(valid_make_dir_args):
-    return {'company_code': 'AAPL', 'cik': '0000320193',
-            'priorto': '20010101', 'filing_type': '10-Q', 'count': 1}
+    return {"company_code": "AAPL", "cik": "0000320193",
+            "priorto": "20010101", "filing_type": "10-Q", "count": 1}
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def crawler():
     return SecCrawler()
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def filing():
-    return Filings('0000320193', "10q", count=3)
+    return Filing("0000320193", "10q", count=3)
