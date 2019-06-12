e4760e322fc3e5b476a0041aaba9c8c0424dcd96
Raffaele Mancuso <raffamaiden@gmail.com>
2019-06-13 01:26:56 +0200

    Update SECEdgar/tests/test_extractor.py
    mis-spelled test
    
    Co-Authored-By: Jack Moody <jacklaytonmoody@gmail.com>

--- SECEdgar/tests/test_extractor.py
+++ SECEdgar/tests/test_extractor.py
@@ -6,7 +6,7 @@ from SECEdgar.extractor import EDGARExtractor
 from SECEdgar.utils.exceptions import FilingTypeError
 
 class TestExtractor(object):
-    def testt_appl_10k(self):
+    def test_aapl_10k(self):
         f = Filing(cik='0000320193', filing_type='10-k')
         metadata = f.save("./data/original", True, "./data/extracted")
         if not metadata:
