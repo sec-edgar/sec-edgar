2670ed941b338fd9c46da2811d3e0ba2c09b373a
jackmoody11 <jackmoody@unc.edu>
2019-12-20 15:31:52 -0500

    Flake8

--- SECEdgar/tests/test_cik.py
+++ SECEdgar/tests/test_cik.py
@@ -17,12 +17,12 @@ class TestCIK(object):
         for company_name, cik in single_result_companies.items():
             if returned_ciks[company_name] != single_result_companies[company_name]:
                 raise AssertionError("CIK for {0} expected "
-                                     "{1}, got {2}".format(company_name, cik, returned_ciks[company_name]))
+                                     "{1}, got {2}".format(company_name,
+                                                           cik, returned_ciks[company_name]))
 
     @pytest.mark.skip(reason='This feature is currently in progress.')
     def test_multiple_results_company_name_search(self, multiple_result_companies):
         # TODO: Add support for when lookup returns multiple companies
-        returned_ciks = CIK(multiple_result_companies).lookup_dict
         for company_name in multiple_result_companies:
             pass
 
