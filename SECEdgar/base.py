1ac8ea4432c62baf39096c4c7ca1a0eab89f7f71
jackmoody11 <jackmoody@unc.edu>
2019-12-23 13:44:00 -0500

    Use lxml as default parser

--- SECEdgar/base.py
+++ SECEdgar/base.py
@@ -40,7 +40,7 @@ class _EDGARBase(ABC):
         return self._client
 
     def get_soup(self):
-        return BeautifulSoup(self.get_response().text, features="html.parser")
+        return BeautifulSoup(self.get_response().text)
 
     def get_response(self):
         return self._client.get_response(self.url, self.params)
