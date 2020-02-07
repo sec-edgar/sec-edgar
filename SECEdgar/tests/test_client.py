6cb39fa0df6e62e45a2e4bc632dd37e698f16fe4
jackmoody11 <jackmoody@unc.edu>
2020-02-06 20:42:29 -0500

    DOC: Fix docstring for datapath

--- SECEdgar/tests/test_client.py
+++ SECEdgar/tests/test_client.py
@@ -15,7 +15,7 @@ def datapath(*args):
     """Get the path to a data file.
 
     Returns:
-        path including ``SECEdgar/tests``.
+        path including ``SECEdgar/tests/data``.
     """
     base_path = os.path.join(os.path.dirname(__file__), 'data')
     return os.path.join(base_path, *args)
