7153a4f0849a0d846f79420ec965c3bb07ea0d14
jackmoody11 <jackmoody@unc.edu>
2020-02-27 17:40:21 -0500

    TEST: Update tests to use monkeypatching

--- SECEdgar/tests/test_cik.py
+++ SECEdgar/tests/test_cik.py
@@ -10,43 +10,51 @@ from SECEdgar.utils.exceptions import EDGARQueryError
 class MockSingleCIKLookupResponse:
     def __init__(self, *args):
         self.status_code = 200
-        with open(datapath("CIK", "single_cik_search_result.html")) as f:
+        with open(datapath("CIK", "single_cik_search_result.html"), 'rb') as f:
+            self.text = f.read()
+
+
+class MockSingleCIKMultipleResultsResponse:
+    def __init__(self, *args):
+        self.status_code = 200
+        with open(datapath("CIK", "cik_multiple_results.html"), 'rb') as f:
+            self.text = f.read()
+
+
+class MockSingleCIKNotFound:
+    def __init__(self, *args):
+        self.status_code = 200
+        with open(datapath("CIK", "cik_not_found.html"), 'rb') as f:
             self.text = f.read()
 
 
 class TestCIK(object):
-    def test_cik_returns_correct_values(self, valid_companies):
-        returned_ciks = CIK(valid_companies.keys()).lookup_dict
-        for ticker, cik in valid_companies.items():
-            if returned_ciks[ticker] != cik:
-                raise AssertionError("CIK for {0} expected "
-                                     "{1}, got {2}".format(ticker, cik, returned_ciks[ticker]))
-
-    def test_cik_company_name(self, single_result_companies):
-        returned_ciks = CIK(single_result_companies.keys()).lookup_dict
-        for company_name, cik in single_result_companies.items():
-            if returned_ciks[company_name] != single_result_companies[company_name]:
-                raise AssertionError("CIK for {0} expected "
-                                     "{1}, got {2}".format(company_name,
-                                                           cik, returned_ciks[company_name]))
-
-    def test_multiple_results_company_name_search(self, monkeypatch, multiple_result_companies):
+    def test_cik_returns_correct_values(self, monkeypatch):
+        aapl = CIK('aapl')
         monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKLookupResponse)
-        assert len(CIK(multiple_result_companies).ciks) == 0
+        assert aapl.ciks == ['0000320193']
 
-    def test_multiple_results_raises_warnings(self, multiple_result_companies):
+    def test_multiple_results_company_name_search(self, monkeypatch):
+        multiple_results_cik = CIK('paper')
+        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKMultipleResultsResponse)
+        assert len(multiple_results_cik.ciks) == 0
+
+    def test_multiple_results_raises_warnings(self, monkeypatch):
+        multiple_results_cik = CIK('paper')
+        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKMultipleResultsResponse)
         with pytest.warns(UserWarning):
-            CIK(multiple_result_companies).ciks()
+            cik = multiple_results_cik.ciks
 
-    def test_validate_cik(self):
-        # string remains unchecked until query to allow for possibility of
-        # using company name, ticker, or CIK as string
-        with pytest.raises(EDGARQueryError):
-            CIK('0notvalid0').ciks()
-        with pytest.raises(EDGARQueryError):
-            CIK('012345678910').ciks()
+    def test_validate_cik_is_string(self):
         # float and int not accepted, raising TypeError
         with pytest.raises(TypeError):
             CIK(1234567891011)
         with pytest.raises(TypeError):
             CIK(123.0)
+
+    def test_validate_cik_after_cik_lookup(self, monkeypatch):
+        # string remains unchecked until query to allow for possibility of
+        # using company name, ticker, or CIK as string
+        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKNotFound)
+        with pytest.raises(EDGARQueryError):
+            cik = CIK('0notvalid0').ciks
--- SECEdgar/tests/test_filings.py
+++ SECEdgar/tests/test_filings.py
@@ -3,12 +3,22 @@ import datetime
 import pytest
 import requests
 
+from SECEdgar.client import NetworkClient
 from SECEdgar.filings import Filing, FilingType, CIK
 
+from SECEdgar.tests.utils import datapath
 from SECEdgar.utils.exceptions import FilingTypeError, EDGARQueryError
 
 
+class MockSingleCIKNotFound:
+    def __init__(self, *args):
+        self.status_code = 200
+        with open(datapath("CIK", "cik_not_found.html"), 'rb') as f:
+            self.text = f.read()
+
+
 class TestFiling(object):
+    @pytest.mark.slow
     def test_count_returns_exact(self, valid_filing_10k):
         urls = valid_filing_10k.get_urls()
         if len(urls) != valid_filing_10k.client.count:
@@ -18,16 +28,17 @@ class TestFiling(object):
 
     def test_date_is_sanitized(self, valid_filing_10k):
         date = datetime.datetime(2015, 1, 1)
-        valid_filing_10k.dateb = date
-        if not valid_filing_10k.dateb == '20150101':
-            raise AssertionError("The dateb param was not correctly sanitized.")
+        valid_filing_10k.end_date = date
+        if not valid_filing_10k.end_date == '20150101':
+            raise AssertionError("The end date was not correctly sanitized.")
 
     def test_date_is_sanitized_when_changed(self, valid_filing_10k):
-        valid_filing_10k.dateb = datetime.datetime(2016, 1, 1)
-        if not valid_filing_10k.dateb == '20160101':
-            raise AssertionError("The dateb param was not correctly sanitized after change.")
+        valid_filing_10k.end_date = datetime.datetime(2016, 1, 1)
+        if not valid_filing_10k.end_date == '20160101':
+            raise AssertionError("The end date was not correctly sanitized after change.")
 
     # TODO: Monkeypatch with example response
+    @pytest.mark.slow
     def test_txt_urls(self, valid_filing_10k):
         r = requests.get(valid_filing_10k.get_urls()[0])
         if not r.text:
@@ -42,27 +53,28 @@ class TestFiling(object):
             with pytest.raises(FilingTypeError):
                 Filing(cik='0000320193', filing_type=t)
 
-    def test_validate_cik(self):
-        # string remains unchecked until query to allow for possibility of
-        # using company name, ticker, or CIK as string
-        with pytest.raises(EDGARQueryError):
-            Filing(cik='0notvalid0', filing_type=FilingType.FILING_10K)
-        with pytest.raises(EDGARQueryError):
-            Filing(cik='012345678910', filing_type=FilingType.FILING_10K)
-        # float and int not accepted, raising TypeError from CIK Validator
+    def test_validate_cik_type_inside_filing(self):
         with pytest.raises(TypeError):
             Filing(cik=1234567891011, filing_type=FilingType.FILING_10K)
         with pytest.raises(TypeError):
             Filing(cik=123.0, filing_type=FilingType.FILING_10K)
 
+    def test_validate_cik_inside_filing(self, monkeypatch):
+        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKNotFound)
+        with pytest.raises(EDGARQueryError):
+            _ = Filing(cik='0notvalid0', filing_type=FilingType.FILING_10K).ciks
+
+    @pytest.mark.slow
     def test_filing_save_multiple_ciks(self, multiple_valid_ciks, tmp_data_directory):
         f = Filing(multiple_valid_ciks, FilingType.FILING_10Q, count=3)
         f.save(tmp_data_directory)
 
+    @pytest.mark.slow
     def test_filing_save_single_cik(self, single_valid_cik, tmp_data_directory):
         f = Filing(single_valid_cik, FilingType.FILING_10Q, count=3)
         f.save(tmp_data_directory)
 
+    @pytest.mark.slow
     def test_filing_get_urls_returns_single_list_of_urls(self):
         ciks = CIK(['aapl', 'msft', 'amzn'])
         f = Filing(ciks, FilingType.FILING_10Q, count=3)
