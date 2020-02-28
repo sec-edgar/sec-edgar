59e10d7403adc31d2ab3c1b34828978410e4093a
jackmoody11 <jackmoody@unc.edu>
2020-02-28 11:30:41 -0500

    CLN: Get rid of unused test fixtures

--- SECEdgar/tests/conftest.py
+++ SECEdgar/tests/conftest.py
@@ -30,57 +30,6 @@ def crawler():
     yield _crawler
 
 
-@pytest.fixture(scope="class")
-def valid_filing_10k():
-    return Filing(aapl_cik, FilingType.FILING_10K, count=3)
-
-
-@pytest.fixture(scope="class")
-def valid_filing_10q():
-    return Filing(aapl_cik, FilingType.FILING_10Q, count=3)
-
-
-@pytest.fixture(scope="class")
-def valid_filing_8k():
-    return Filing(aapl_cik, FilingType.FILING_8K, count=3)
-
-
-@pytest.fixture(scope="class")
-def valid_filing_13f():
-    return Filing(aapl_cik, FilingType.FILING_13F, count=3)
-
-
-@pytest.fixture(scope="class")
-def valid_companies():
-    return {'aapl': '0000320193',
-            'msft': '0000789019',
-            'amzn': '0001018724',
-            'fb': '0001326801'}
-
-
-@pytest.fixture(scope="class")
-def single_result_companies():
-    return {'Apple Inc.': '0000320193',
-            'Microsoft Corp': '0000789019',
-            'Amazon Com Inc': '0001018724',
-            'Facebook': '0001326801'}
-
-
-@pytest.fixture(scope="class")
-def multiple_result_companies():
-    return 'paper', 'company'
-
-
-@pytest.fixture(scope="class")
-def multiple_valid_ciks():
-    return CIK(['aapl', 'msft', 'amzn', 'fb'])
-
-
-@pytest.fixture(scope="class")
-def single_valid_cik():
-    return CIK('aapl')
-
-
 @pytest.fixture(scope="session")
 def tmp_data_directory(tmpdir_factory):
     return str(tmpdir_factory.mktemp("tmp_data"))
