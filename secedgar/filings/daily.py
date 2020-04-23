1d9643a9eee6a4a3714c0db14540a6ce99a54a4b
jackmoody11 <jackmoody@unc.edu>
2020-04-23 13:12:44 -0400

    FIX: Implement all abstract methods

--- secedgar/filings/daily.py
+++ secedgar/filings/daily.py
@@ -5,10 +5,15 @@ from secedgar.client import NetworkClient
 
 
 class DailyFilings(AbstractFiling):
-    """Class for retrieving all daily filings from https://www.sec.gov/Archives/edgar/daily-index."""
+    """Class for retrieving all daily filings from https://www.sec.gov/Archives/edgar/daily-index.
+
+    Attributes:
+        date (datetime.datetime): Date of daily filing to fetch.
+        client (secedgar.client._base.AbstractClient): Client to use for fetching data.
+            Defaults to ``secedgar.client.NetworkClient`` if none is given.
+    """
 
     def __init__(self, date, client=None):
-        super().__init__()
         if not isinstance(date, datetime.datetime):
             raise TypeError(
                 "Date must be given as datetime object. Was given type {t}.".format(type(date)))
@@ -20,7 +25,7 @@ class DailyFilings(AbstractFiling):
         return self._client
 
     def _get_quarter(self):
-        """Get quarter number from date given."""
+        """Get quarter number from date attribute."""
         return (self._date.month - 1) // 3 + 1
 
     @property
@@ -33,4 +38,10 @@ class DailyFilings(AbstractFiling):
         return {}
 
     def get_urls(self, **kwargs):
+        response = self.client.get_response(self.path, self.params, **kwargs)
+        idx_file = response.text
+        # with open(idx_file, 'r') as f:
+        # for line in f.readlines()
+
+    def save(self, directory):
         pass
