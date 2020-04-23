27d7e94fa0066681ba8f979705387a24c31cbb02
jackmoody11 <jackmoody@unc.edu>
2020-04-23 13:13:03 -0400

    TEST: Add testing for `get_quarter`

---
+++ secedgar/tests/filings/test_daily.py
@@ -0,0 +1,23 @@
+import pytest
+
+from datetime import datetime
+
+from secedgar.filings.daily import DailyFilings
+
+
+class TestDaily:
+    @pytest.mark.parametrize(
+        "date,expected",
+        [
+            (datetime(2020, 1, 1), 1),
+            (datetime(2020, 3, 31), 1),
+            (datetime(2020, 4, 1), 2),
+            (datetime(2020, 6, 30), 2),
+            (datetime(2020, 7, 1), 3),
+            (datetime(2020, 9, 30), 3),
+            (datetime(2020, 10, 1), 4),
+            (datetime(2020, 12, 31), 4)
+        ]
+    )
+    def test_get_quarter(self, date, expected):
+        assert DailyFilings(date=date)._get_quarter() == expected
