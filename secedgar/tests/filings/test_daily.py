7dae0a4c788f45bca4da9ae0ac6d8d83ea331060
jackmoody11 <jackmoody@unc.edu>
2020-05-06 11:35:40 -0400

    TEST: Add mock for master idx file

--- secedgar/tests/filings/test_daily.py
+++ secedgar/tests/filings/test_daily.py
@@ -7,9 +7,20 @@ from secedgar.tests.utils import datapath
 
 
 class MockQuarterDirectory:
+    """Mock response object for all 2018 daily listings of quarter 4. """
+
     def __init__(self, *args):
         self.status_code = 200
         with open(datapath("filings", "daily", "daily_index_2018_QTR4.htm")) as f:
+            self.text = f.read()
+
+
+class MockMasterIdxFile:
+    """Mock response object for master.20181231.idx file. """
+
+    def __init__(self, *args):
+        self.status_code = 200
+        with open(datapath("filings", "daily", "master.20181231.idx")) as f:
             self.text = f.read()
 
 
@@ -38,3 +49,21 @@ class TestDaily:
         assert DailyFilings(datetime(2018, 12, 31))._get_quarterly_directory().status_code == 200
         assert "master.20181231.idx" in DailyFilings(
             datetime(2018, 12, 31))._get_quarterly_directory().text
+
+    @pytest.mark.parametrize(
+        "company_name",
+        [
+            "HENRY SCHEIN INC",
+            "ROYAL BANK OF CANADA",
+            "NOVAVAX INC",
+            "BROOKFIELD ASSET MANAGEMENT INC.",
+            "PERUSAHAAN PERSEROAN PERSERO PT TELEKOMUNIKASI INDONESIA TBK"
+        ]
+    )
+    def test_get_master_idx_file(self, monkeypatch, company_name):
+        daily_filing = DailyFilings(datetime(2018, 12, 31))
+        monkeypatch.setattr(DailyFilings, "_get_quarterly_directory", MockQuarterDirectory)
+        monkeypatch.setattr(DailyFilings, "_get_master_idx_file", MockMasterIdxFile)
+
+        # All company names above should be in file
+        assert company_name in daily_filing._get_master_idx_file().text
