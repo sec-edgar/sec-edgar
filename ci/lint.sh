ff2202602d90e0a7da1c8de1250086a3c85039a5
jackmoody11 <jackmoody@unc.edu>
2020-05-17 23:06:57 -0400

    CI: Ignore tests for docstring check

--- ci/lint.sh
+++ ci/lint.sh
@@ -7,9 +7,9 @@
 #
 # Usage:
 #     $ ./ci/lint.sh    # flake8 check
-
 echo "Using flake8 version $(flake8 --version)"
 flake8 secedgar --count --show-source --statistics
 
 echo "Running docstring style check"
-pydocstyle secedgar/ --convention=google --count --add-ignore=D100,D104,D105,D107
+# Ignore tests directory
+pydocstyle secedgar/ --convention=google --count --add-ignore=D100,D104,D105,D107 --match-dir='[^\.][^tests].*'
