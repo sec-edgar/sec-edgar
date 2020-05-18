33e699b4e825c2d2269daa589f5b28d84512d00b
jackmoody11 <jackmoody@unc.edu>
2020-05-18 17:59:30 -0400

    CI: Add coverage option to tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,6 +3,20 @@
 # Runs pytest tests for project
 #
 # Usage:
-#     $ ./ci/run_tests.sh    # Runs all tests
+#     $ ./ci/run_tests.sh       # Runs all tests
+#     $ ./ci/run_tests.sh cover # Runs all tests with coverage
 echo "Running tests"
-pytest secedgar/tests
+
+if [[ -z "$1" ]]; then
+    pytest secedgar/tests
+elif [[ "$1" -eq "cover" ]]; then
+    # run tests, omit tests from coverage
+    coverage run --source secedgar --omit=*/tests* -m pytest
+    # output coverage results to xml file
+    coverage xml -o cov.xml
+    # Show missing lines
+    coverage report -m
+else
+    echo "Arguments given not recognized."
+    exit 1
+fi
