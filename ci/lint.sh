47035e14f2be72ea6b2c4bf739b555ba63328396
jackmoody11 <jackmoody@unc.edu>
2020-05-18 12:07:15 -0400

    CI: Add docstring argument

--- ci/lint.sh
+++ ci/lint.sh
@@ -6,10 +6,13 @@
 # linting standards are upheld.
 #
 # Usage:
-#     $ ./ci/lint.sh    # flake8 check
+#     $ ./ci/lint.sh             # flake8 check
+#     $ ./ci/lint.sh docstring   # flake8 and docstring check
 echo "Using flake8 version $(flake8 --version)"
 flake8 secedgar --count --show-source --statistics
 
-echo "Running docstring style check"
-# Ignore tests directory
-pydocstyle secedgar/ --convention=google --count --add-ignore=D100,D104,D105,D107 --match-dir='[^\.][^tests].*'
+if [[ "$1" -eq "docstring" ]]; then
+    echo "Running docstring style check"
+    # Ignore tests directory
+    pydocstyle secedgar/ --convention=google --count --add-ignore=D100,D104,D105,D107 --match-dir='[^\.][^tests].*'
+fi
