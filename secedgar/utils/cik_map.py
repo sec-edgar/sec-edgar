739fac4554c09945494f103d1c64ed6d5cd85b52
jackmoody11 <jackmoody@unc.edu>
2020-05-20 16:18:00 -0400

    DOC: Improve docstrings

--- secedgar/utils/cik_map.py
+++ secedgar/utils/cik_map.py
@@ -4,11 +4,20 @@ URL = "https://www.sec.gov/files/company_tickers.json"
 
 
 def get_cik_map():
-    """Get map of tickers to CIK numbers."""
+    """Get dictionary of tickers to CIK numbers.
+
+    Returns:
+        Dictionary with tickers being keys and CIKs, as strings, being the values.
+    """
     response = requests.get(URL).json()
     return {v["ticker"]: str(v["cik_str"]) for _, v in response.items()}
 
 
 def get_company_name_map():
+    """Get dictionary of tickers to CIK numbers.
+
+    Returns:
+        Dictionary with company names being keys and CIKs, as strings, being the values.
+    """
     response = requests.get(URL).json()
     return {v["title"]: str(v["cik_str"]) for _, v in response.items()}
