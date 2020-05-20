b0a48f30c63057ced160ba361bbe2c23381794ad
jackmoody11 <jackmoody@unc.edu>
2020-05-20 15:55:23 -0400

    -a

--- secedgar/utils/cik_map.py
+++ secedgar/utils/cik_map.py
@@ -1,8 +1,14 @@
 import requests
 
+URL = "https://www.sec.gov/files/company_tickers.json"
+
 
 def get_cik_map():
     """Get map of tickers to CIK numbers."""
-    url = "https://www.sec.gov/files/company_tickers.json"
-    response = requests.get(url).json()
+    response = requests.get(URL).json()
     return {v["ticker"]: str(v["cik_str"]) for _, v in response.items()}
+
+
+def get_company_name_map():
+    response = requests.get(URL).json()
+    return {v["title"]: str(v["cik_str"]) for _, v in response.items()}
