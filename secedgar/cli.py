d5408c3976ddf7eeb33d9a8c0a90fab3ae51568e
jackmoody11 <jackmoody@unc.edu>
2020-06-29 17:17:51 -0400

    MAINT: flake8

--- secedgar/cli.py
+++ secedgar/cli.py
@@ -1,7 +1,8 @@
 import click
 from datetime import datetime
 import os
-from secedgar.filings import Filing, DailyFilings
+from secedgar.filings import Filing, DailyFilings, FilingType
+from secedgar.utils.exceptions import FilingTypeError
 
 
 @click.group()
@@ -14,18 +15,22 @@ def date_cleanup(date):
     """Transforms date of form YYYYMMDD to datetime object.
 
     Args:
-        date (str): Date of the form YYYYMMDD to be transformed.
+        date (Union[str, NoneType]): Date of the form YYYYMMDD to be transformed.
 
     Returns:
-        ``datetime.datetime`` object.
+        ``datetime.datetime`` object if given string.
+        Returns None if None is given.
     """
-    return datetime.strptime(date, "%Y%m%d")
+    return datetime.strptime(date, "%Y%m%d") if date is not None else None
 
 
 @cli.command()
 @click.option('-l', '--lookups',
-              help='Companies and tickers to include in filing download.', required=True)
-@click.option('-t', '--ftype', help='Choose a filing type.')  # Need to convert this to enum somehow
+              help='Companies and tickers to include in filing download.',
+              required=True,
+              multiple=True)
+@click.option('-t', '--ftype', help='Choose a filing type.',
+              required=True)  # Need to convert this to enum somehow
 @click.option('-s', '--start',
               help="""Start date for filings.
               Should be in the format YYYYMMDD. Defaults to first available filing.""",
@@ -41,6 +46,12 @@ def date_cleanup(date):
               default=os.getcwd(), type=str)
 def filing(lookups, ftype, start, end, count, directory):
     """Click command for downloading filings. Run ``secedgar filing --help`` for info."""
+    # If given filing type is not valid enum, raise FilingTypeError
+    try:
+        ftype = FilingType[ftype]
+    except KeyError:
+        raise FilingTypeError()
+
     f = Filing(cik_lookup=lookups,
                filing_type=ftype,
                start_date=date_cleanup(start),
