9e8298b5935d13d4d3be70695c0a5ab43811cfe7
jackmoody11 <jackmoody@unc.edu>
2020-07-21 10:45:28 -0400

    STRUCT: Add MasterFilings to filings package

--- secedgar/filings/__init__.py
+++ secedgar/filings/__init__.py
@@ -2,3 +2,4 @@ from secedgar.filings.filing import Filing  # noqa:F401
 from secedgar.filings.cik_lookup import CIKLookup  # noqa: F401
 from secedgar.filings.filing_types import FilingType  # noqa:F401
 from secedgar.filings.daily import DailyFilings  # noqa:F401
+from secedgar.filings.master import MasterFilings  # noqa:F401
