9acf5a0063d8ab12e23ebd70e851b6b64d2b946f
jackmoody11 <jackmoody@unc.edu>
2020-07-22 16:00:30 -0400

    CLN: Convert mock classes to fixtures

--- secedgar/tests/filings/test_cik_lookup.py
+++ secedgar/tests/filings/test_cik_lookup.py
@@ -4,49 +4,47 @@ from secedgar.tests.utils import datapath
 
 from secedgar.filings import CIKLookup
 from secedgar.client import NetworkClient
+from secedgar.tests.conftest import MockResponse
 from secedgar.utils.exceptions import EDGARQueryError
 
 
-class MockSingleCIKLookupResponse:
-    def __init__(self, *args):
-        self.status_code = 200
-        with open(datapath("CIK", "single_cik_search_result.html"), 'rb') as f:
-            self.text = f.read()
+@pytest.fixture
+def mock_single_cik_lookup_response(monkeypatch):
+    def _mock_single_cik_lookup_response(*args, **kwargs):
+        return MockResponse(datapath_args=["CIK", "single_cik_search_result.html"], file_read_args="rb")
+    monkeypatch.setattr(NetworkClient, "get_response", _mock_single_cik_lookup_response)
 
 
-class MockSingleCIKMultipleResultsResponse:
-    def __init__(self, *args):
-        self.status_code = 200
-        with open(datapath("CIK", "cik_multiple_results.html"), 'rb') as f:
-            self.text = f.read()
+@pytest.fixture
+def mock_single_cik_multiple_results_response(monkeypatch):
+    def _mock_single_cik_multiple_results_response(*args, **kwargs):
+        return MockResponse(datapath_args=["CIK", "cik_multiple_results.html"], file_read_args="rb")
+    monkeypatch.setattr(NetworkClient, "get_response", _mock_single_cik_multiple_results_response)
 
 
-class MockSingleCIKNotFound:
-    def __init__(self, *args):
-        self.status_code = 200
-        with open(datapath("CIK", "cik_not_found.html"), 'rb') as f:
-            self.text = f.read()
+@pytest.fixture
+def mock_single_cik_not_found(monkeypatch):
+    def _mock_single_cik_not_found(*args, **kwargs):
+        return MockResponse(datapath_args=["CIK", "cik_not_found.html"], file_read_args="rb")
+    monkeypatch.setattr(NetworkClient, "get_response", _mock_single_cik_not_found)
 
 
 class TestCIKLookup(object):
-    def test_cik_lookup_returns_correct_values(self, monkeypatch):
+    def test_cik_lookup_returns_correct_values(self, mock_single_cik_lookup_response):
         aapl = CIKLookup('aapl')
-        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKLookupResponse)
         assert aapl.ciks == ['0000320193']
 
     def test_cik_lookup_lookups_property(self):
         multiple_company_lookup = CIKLookup(['aapl', 'msft', 'fb'])
         assert multiple_company_lookup.lookups == ['aapl', 'msft', 'fb']
 
-    def test_multiple_results_company_name_search(self, monkeypatch):
+    def test_multiple_results_company_name_search(self, mock_single_cik_multiple_results_response):
         multiple_results_cik = CIKLookup('paper')
-        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKMultipleResultsResponse)
         with pytest.warns(UserWarning):
             assert len(multiple_results_cik.ciks) == 0
 
-    def test_multiple_results_raises_warnings(self, monkeypatch):
+    def test_multiple_results_raises_warnings(self, mock_single_cik_multiple_results_response):
         multiple_results_cik = CIKLookup('paper')
-        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKMultipleResultsResponse)
         with pytest.warns(UserWarning):
             _ = multiple_results_cik.ciks
 
@@ -57,13 +55,12 @@ class TestCIKLookup(object):
             123.0,
         ]
     )
-    def test_validate_cik__is_string(self, bad_cik, ):
+    def test_validate_cik__is_string(self, bad_cik):
         with pytest.raises(TypeError):
             CIKLookup(bad_cik)
 
-    def test_validate_cik_after_cik_lookup(self, monkeypatch):
+    def test_validate_cik_after_cik_lookup(self, mock_single_cik_not_found):
         # string remains unchecked until query to allow for possibility of
         # using company name, ticker, or CIK as string
-        monkeypatch.setattr(NetworkClient, "get_response", MockSingleCIKNotFound)
         with pytest.raises(EDGARQueryError):
             _ = CIKLookup('0notvalid0').ciks
