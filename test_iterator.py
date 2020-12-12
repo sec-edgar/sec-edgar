512b816714fd38720e55bc02ed7e6dc064b6f1d0
Peter Stenger <peter.a.stenger@gmail.com>
2020-12-11 22:39:26 -0500

    added threading for file compression

--- test_iterator.py
+++ test_iterator.py
@@ -6,6 +6,4 @@ import secedgar.filings.daily as d
 
 date = datetime(2003,7,15)
 inst = d.DailyFilings(date)
-for i in range(3):
-    print('RUN', i+1)
-    inst.save('./out')
+inst.save('./out', download_all=True)
