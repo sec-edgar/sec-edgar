b1ec7aad8dbf8d20df08e52752eb39d0aeb8f69c
jackmoody11 <jacklaytonmoody@gmail.com>
2020-12-13 20:51:39 -0500

    STRUCT: Make object variables "private"

--- secedgar/client/async_session.py
+++ secedgar/client/async_session.py
@@ -14,30 +14,30 @@ class RateLimitedClientSession:
     """
 
     def __init__(self, client, rate_limit):
-        self.client = client
-        self.rate_limit = rate_limit
-        self.max_tokens = rate_limit
-        self.tokens = self.max_tokens
-        self.updated_at = time.monotonic()
-        self.start = time.monotonic()
+        self._client = client
+        self._rate_limit = rate_limit
+        self._max_tokens = rate_limit
+        self._tokens = self._max_tokens
+        self._updated_at = time.monotonic()
+        self._start = time.monotonic()
 
     async def get(self, *args, **kwargs):
         """Wrapper for ``client.get`` that first waits for a token."""
         await self.wait_for_token()
-        return self.client.get(*args, **kwargs)
+        return self._client.get(*args, **kwargs)
 
     async def wait_for_token(self):
         """Sleeps until a new token is added."""
-        while self.tokens < 1:
+        while self._tokens < 1:
             self.add_new_tokens()
             await asyncio.sleep(0.03)
-        self.tokens -= 1
+        self._tokens -= 1
 
     def add_new_tokens(self):
         """Adds a new token if time elapsed is greater than minimum time."""
         now = time.monotonic()
-        time_since_update = now - self.updated_at
-        new_tokens = time_since_update * self.rate_limit
-        if self.tokens + new_tokens >= 1:
-            self.tokens = min(self.tokens + new_tokens, self.max_tokens)
-            self.updated_at = now
+        time_since_update = now - self._updated_at
+        new_tokens = time_since_update * self._rate_limit
+        if self._tokens + new_tokens >= 1:
+            self._tokens = min(self._tokens + new_tokens, self._max_tokens)
+            self._updated_at = now
