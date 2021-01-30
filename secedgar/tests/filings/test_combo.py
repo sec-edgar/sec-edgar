8f40945aaa9f2e6062e2f4ef9e2f4f430d91243b
Peter Stenger <peter.a.stenger@gmail.com>
2021-01-30 18:04:51 -0500

    continue fixing tests

--- secedgar/tests/filings/test_combo.py
+++ secedgar/tests/filings/test_combo.py
@@ -2,53 +2,67 @@ from datetime import date
 
 import pytest
 from secedgar.filings.combo import ComboFilings
-
-
+import logging
+def lambda_matches(a, b):
+    matches = a.__code__.co_code == b.__code__.co_code
+    if not matches:
+        logging.warning('Did not match')
+    return matches
+def line_matches(a, b):
+    matches = a[0] == b[0] and a[1] == b[1] and lambda_matches(a[2], b[2])
+    return matches
+def master_list_matches(l1, l2):
+    return all(line_matches(a, b) for (a,b) in zip(l1, l2))
 class TestComboFilings:
     def test_combo_quarterly_only_one_year(self):
         combo = ComboFilings(start_date=date(2020, 1, 1), end_date=date(2020, 12, 31))
-        expected = [(2020, 1), (2020, 2), (2020, 3), (2020, 4)]
-        assert combo.master_date_list == expected and not combo.daily_date_list
+        expected = [(2020, 1, lambda _: True), (2020, 2, lambda _: True), (2020, 3, lambda _: True), (2020, 4, lambda _: True)]
+        assert master_list_matches(combo.master_date_list, expected)
+        assert len(combo.daily_date_list) == 0
 
     def test_combo_quarterly_only_multiple_years(self):
-        combo = ComboFilings(start_date=date(2018, 9, 1), end_date=date(2020, 6, 30))
+        combo = ComboFilings(start_date=date(2018, 10, 1), end_date=date(2020, 6, 30))
         expected = [
-            (2018, 3),
-            (2018, 4),
-            (2019, 1),
-            (2019, 2),
-            (2019, 3),
-            (2019, 4),
-            (2020, 1),
-            (2020, 2),
+            (2018, 4, lambda _: True),
+            (2019, 1, lambda _: True),
+            (2019, 2, lambda _: True),
+            (2019, 3, lambda _: True),
+            (2019, 4, lambda _: True),
+            (2020, 1, lambda _: True),
+            (2020, 2, lambda _: True),
         ]
-        assert combo.master_date_list == expected and not combo.daily_date_list
+        logging.info(f'Expected: {expected}')
+        logging.info(f'Actual: {combo.master_date_list}')
+        assert master_list_matches(combo.master_date_list, expected)
+        assert len(combo.daily_date_list) == 0
 
     def test_combo_daily_only_single_day(self):
         combo = ComboFilings(start_date=date(2020, 12, 10), end_date=date(2020, 12, 10))
-        assert combo.daily_date_list == ["20201210"] and not combo.master_date_list
+        assert [str(s) for s in combo.daily_date_list] == ["2020-12-10"]
+        assert len(combo.master_date_list) == 0
 
     def test_combo_daily_only_multiple_days(self):
         combo = ComboFilings(start_date=date(2020, 12, 10), end_date=date(2020, 12, 12))
-        expected = ["20201210", "20201211", "20201212"]
-        assert combo.daily_date_list == expected and not combo.master_date_list
+        expected = ["2020-12-10", "2020-12-11", "2020-12-12"]
+        assert [str(s) for s in combo.daily_date_list] == expected
+        assert len(combo.master_date_list) == 0
 
     @pytest.mark.parametrize(
         "start_date,end_date,quarterly_expected,daily_expected",
         [
-            (date(2019, 12, 28), date(2020, 4, 1), [(2020, 1)], ["20191228",
-                                                                 "20191229",
-                                                                 "20191230",
-                                                                 "20191231",
-                                                                 "20200401"]),
-            (date(2020, 3, 30), date(2020, 10, 2), [(2020, 2), (2020, 3)], ["20200330",
-                                                                            "20200331",
-                                                                            "20201001",
-                                                                            "20201002"]),
-            (date(2020, 1, 1), date(2020, 4, 2), [(2020, 1)], ["20200401",
-                                                               "20200402"]),
-            (date(2020, 3, 30), date(2020, 9, 30), [(2020, 2), (2020, 3)], ["20200330",
-                                                                            "20200331"]),
+            (date(2019, 12, 28), date(2020, 4, 1), [(2020, 1, lambda _: True)], ["2019-12-28",
+                                                                 "2019-12-29",
+                                                                 "2019-12-30",
+                                                                 "2019-12-31",
+                                                                 "2020-04-01"]),
+            (date(2020, 3, 30), date(2020, 10, 2), [(2020, 2, lambda _: True), (2020, 3, lambda _: True)], ["2020-03-30",
+                                                                            "2020-03-31",
+                                                                            "2020-10-01",
+                                                                            "2020-10-02"]),
+            (date(2020, 1, 1), date(2020, 4, 2), [(2020, 1, lambda _: True)], ["2020-04-01",
+                                                               "2020-04-02"]),
+            (date(2020, 3, 30), date(2020, 9, 30), [(2020, 2, lambda _: True), (2020, 3, lambda _: True)], ["2020-03-30",
+                                                                            "2020-03-31"]),
         ]
     )
     # TODO: Before making daily_date_list, should we make sure there are filings for that date?
@@ -58,5 +72,5 @@ class TestComboFilings:
                                          quarterly_expected,
                                          daily_expected):
         combo = ComboFilings(start_date=start_date, end_date=end_date)
-        assert combo.daily_date_list == daily_expected \
-            and combo.master_date_list == quarterly_expected
+        assert [str(s) for s in combo.daily_date_list] == daily_expected
+        assert master_list_matches(combo.master_date_list, quarterly_expected)
