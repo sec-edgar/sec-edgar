2ecfba463f53b659ff2cfa9e2bba1e69735c227e
jackmoody11 <jackmoody@unc.edu>
2021-04-13 23:11:00 -0400

    TEST: Improve testing for multiple filings saves

--- secedgar/tests/test_cli.py
+++ secedgar/tests/test_cli.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from click.testing import CliRunner
 from secedgar.cli import daily, filing
@@ -35,15 +37,29 @@ class TestCLIFiling:
         check_bad_inputs(filing, user_input, expected_exception, tmp_data_directory)
 
     @pytest.mark.parametrize(
-        "user_input",
+        "user_input,count",
         [
-            "-l aapl msft fb FILING_10Q",
-            "-l aapl msft fb FILING_10Q -n 10",
-            "-l aapl msft fb FILING_10Q -n 1"
+            ("-l aapl -l msft -l amzn -t FILING_10Q -u 'My User Agent (email@example.com)'", None),
+            ("-l aapl -l msft -l amzn -t FILING_10Q -n {} -u 'My User Agent (email@example.com)'", 10),
+            ("-l aapl -l msft -l amzn -t FILING_10Q -n {} -u 'My User Agent (email@example.com)'", 1),
         ]
     )
-    def test_multiple_companies_input(self, user_input, tmp_data_directory):
-        pass
+    def test_multiple_companies_input(self,
+                                      user_input,
+                                      count,
+                                      tmp_data_directory,
+                                      mock_cik_validator_get_multiple_ciks,
+                                      mock_single_cik_filing,
+                                      mock_filing_response):
+        if count is not None:
+            user_input = user_input.format(count)
+        result = run_cli_command(filing, user_input, tmp_data_directory)
+        assert result.exit_code == 0
+        txt_files = [f for *_, files in os.walk(tmp_data_directory) for f in files]
+        if count is None:
+            assert len(txt_files) == 3
+        else:
+            assert len(txt_files) == 3 * count
 
 
 class TestCLIDaily:
