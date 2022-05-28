c0d35b0f0f001a063d4c0e22b817223764d5ba15
Jack Moody <jacklaytonmoody@gmail.com>
2022-05-27 22:11:11 -0400

    ENH: Add ability to get all submissions other than just recent

--- secedgar/core/rest.py
+++ secedgar/core/rest.py
@@ -1,5 +1,3 @@
-from cgitb import lookup
-
 import requests
 from secedgar.cik_lookup import CIKLookup
 
@@ -14,13 +12,53 @@ def _get_lookup_dict(lookups, user_agent):
     return cik_lookup.lookup_dict
 
 
-def get_submissions(lookups, user_agent):
+def _combine_dicts(*dicts):
+    if len(dicts) == 1:
+        return dicts[0]
+    else:
+        first = dicts[0]
+        # add each dict to combined dict
+        for d in dicts[1:]:
+            for k in first.keys():
+                first[k] = first[k] + d[k]
+        return first
+
+
+def get_submissions(lookups, user_agent, recent=True):
+    """Get information about submissions for entities.
+
+    Args:
+        lookups (list of str or str): Tickers or CIKs to get submission data for.
+        user_agent (str): User agent to provide the SEC.
+        recent (bool, optional): Whether or not to only get the most recent.
+            Setting ``recent`` to True will give at least one year of filings or 1000 filings
+            (whichever is more). Setting ``recent`` to False will return all filings.
+            Defaults to True.
+
+    Returns:
+        dict: Dictionary with keys being the lookups and values being the responses from the API.
+    """
     lookup_dict = _get_lookup_dict(lookups=lookups, user_agent=user_agent)
     submissions_dict = dict()
+    url_base = "https://data.sec.gov/submissions/"
     for lookup, cik in lookup_dict.items():
-        resp = requests.get("https://data.sec.gov/submissions/CIK{0}.json".format(cik.zfill(10)),
+        resp = requests.get("{0}CIK{1}.json".format(url_base, cik.zfill(10)),
                             headers={"user-agent": user_agent})
-        submissions_dict[lookup] = resp.json()
+        resp_json = resp.json()
+        if not recent:
+            try:
+                older_submission_files = resp_json["filings"]["files"]
+            except KeyError:
+                pass
+
+            # Get data for older submission files and add to recent
+            older_submissions = [requests.get("{0}{1}".format(url_base, f["name"]),
+                                              headers={"user-agent": user_agent}).json()
+                                 for f in older_submission_files]
+
+            resp_json["filings"]["recent"] = _combine_dicts(resp_json["filings"]["recent"],
+                                                            *older_submissions)
+        submissions_dict[lookup] = resp_json
     return submissions_dict
 
 
@@ -69,8 +107,8 @@ if __name__ == "__main__":
     user_agent = "Nunya Business (nunyabusiness@gmail.com)"
     lookups = ["aapl"]
 
-    # submissions = get_submissions(lookups=lookups, user_agent=user_agent)
-    # print(submissions)
+    submissions = get_submissions(lookups=lookups, user_agent=user_agent, recent=False)
+    print(submissions["aapl"])
 
     # concepts = get_company_concepts(lookups=lookups,
     #                                 user_agent=user_agent,
