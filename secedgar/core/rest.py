ceef04cdc10841126de9ba961ed7a6f674dca738
jackmoody11 <jackmoody@unc.edu>
2022-06-09 23:16:14 -0400

    DOC: Update docstrings and add examples

--- secedgar/core/rest.py
+++ secedgar/core/rest.py
@@ -1,4 +1,5 @@
 import requests
+
 from secedgar.cik_lookup import CIKLookup
 
 API_BASE = "https://data.sec.gov/api/"
@@ -6,6 +7,15 @@ XBRL_BASE = "{0}xbrl/".format(API_BASE)
 
 
 def _get_lookup_dict(lookups, user_agent):
+    """Utility function to get CIKs for lookups.
+
+    Args:
+        lookups (list of str): List of tickers or company names to get CIKs for.
+        user_agent (str): User agent to give to SEC.
+
+    Returns:
+        dict: Dictionary with lookup and CIK key-value pairs.
+    """
     cik_lookup = CIKLookup(lookups=lookups,
                            user_agent=user_agent,
                            client=None)
@@ -13,6 +23,18 @@ def _get_lookup_dict(lookups, user_agent):
 
 
 def _combine_dicts(*dicts):
+    """Utility function to combine dictionaries.
+
+    Returns:
+        dict: Dictionaries combined into one.
+
+    Examples:
+
+        >>> a = {"A": [1, 2, 3], "B": [4, 5, 6]}
+        >>> b = {"A": [7, 8], "B": [0, 1, 2]}
+        >>> _combine_dicts(a, b)
+        {'A': [1, 2, 3, 7, 8], 'B': [4, 5, 6, 0, 1, 2]}
+    """
     if len(dicts) == 1:
         return dicts[0]
     else:
@@ -63,6 +85,26 @@ def get_submissions(lookups, user_agent, recent=True):
 
 
 def get_company_concepts(lookups, user_agent, concept_name):
+    """Get company concepts using SEC's REST API.
+
+    Args:
+        lookups (list of str): Tickers or company names to lookup concepts for.
+
+        user_agent (str): User agent to send to SEC.
+        concept_name (str): Name of the concept to get data for.
+
+    Returns:
+        dict: Dictionary with concept data for given lookups.
+
+    Example:
+
+        .. code::
+
+            concept = "AccountsPayableCurrent"
+            get_company_concepts(lookups=["AAPL"],
+                                 user_agent="Name (email@example.com)",
+                                 concept_name=concept)
+    """
     lookup_dict = _get_lookup_dict(lookups=lookups, user_agent=user_agent)
     company_concepts = dict()
     for lookup, cik in lookup_dict.items():
@@ -78,6 +120,30 @@ def get_company_concepts(lookups, user_agent, concept_name):
 
 
 def get_company_facts(lookups, user_agent):
+    """Get company facts for lookups.
+
+    Args:
+        lookups (list of str): Tickers or company names to lookup company facts for.
+        user_agent (str): User agent to send to SEC.
+
+    Returns:
+        dict: Dictionary with lookups as keys and company fact dictionaries as values.
+
+    Examples:
+
+        >>> facts = get_company_facts(["aapl"], "Name (email@example.com)")
+        >>> single_fact = facts["aapl"]["facts"]["us-gaap"]["Assets"]["units"]["USD"][0]
+        >>> single_fact["val"]
+        39572000000
+        >>> single_fact["fy"]
+        2009
+        >>> single_fact["fp"]
+        Q3
+        >>> single_fact["form"]
+        '10-Q'
+        >>> single_fact["filed"]
+        '2009-07-22'
+    """
     lookup_dict = _get_lookup_dict(lookups=lookups, user_agent=user_agent)
     company_facts = dict()
     for lookup, cik in lookup_dict.items():
@@ -87,28 +153,64 @@ def get_company_facts(lookups, user_agent):
     return company_facts
 
 
-def get_xbrl_frames(lookups, user_agent, concept_name, year, quarter=None, instantaneous=False):
-    lookup_dict = _get_lookup_dict(lookups=lookups, user_agent=user_agent)
-    xbrl_frames = dict()
-    for lookup, cik in lookup_dict.items():
-        # Create URL
-        period = "CY{0}".format(year) if quarter is None else "CY{0}Q{1}".format(year, quarter)
-        if instantaneous:
-            period += "I"
-        url = "{0}frames/us-gaap/{1}/{2}.json".format(XBRL_BASE, concept_name, period)
+def get_xbrl_frames(user_agent,
+                    concept_name,
+                    year,
+                    quarter=None,
+                    currency="USD",
+                    instantaneous=False):
+    """
 
-        # Request and add to dictionary
-        resp = requests.get(url, headers={"user-agent": user_agent})
-        xbrl_frames[lookup] = resp.json()
+    Args:
+        user_agent (str): User agent to send to the SEC.
+        concept_name (str): Concept name to get. For example, "Assets".
+        year (int): Year to get concept data for.
+        quarter (Union[int, NoneType], optional): Quarter to get data for. If given None,
+            will look for data for the entire year. Defaults to None.
+        instantaneous (bool, optional): Whether to look for instantaneous data.
+            See `SEC website for more <https://www.sec.gov/edgar/sec-api-documentation>`_.
+            Defaults to False.
+
+    Returns:
+        dict: Dictionary with information about concept_name.
+            Dig into the data key for all period data.
+
+    Examples:
+
+        .. code::
+
+            frames = get_xbrl_frames(user_agent="Name (email@example.com)",
+                                     concept_name="Assets",
+                                     year=2020,
+                                     quarter=3,
+                                     instantaneous=True)
+            print(frames["data"][-1]["entityName"])
+            # Prints "MOXIAN (BVI) INC"
+            print(frames["data"][-1]["val"])
+            # Prints 2295657
+            print(frames["data"][-1]["accn"])
+            # Prints "0001493152-22-013323"
+    """
+    # Create URL
+    period = "CY{0}".format(year) if quarter is None else "CY{0}Q{1}".format(year, quarter)
+    if instantaneous:
+        period += "I"
+    url = "{0}frames/us-gaap/{1}/{2}/{3}.json".format(XBRL_BASE, concept_name, currency, period)
+    print(url)
+    # Request and add to dictionary
+    resp = requests.get(url, headers={"user-agent": user_agent})
+    xbrl_frames = resp.json()
     return xbrl_frames
 
 
 if __name__ == "__main__":
-    user_agent = "Nunya Business (nunyabusiness@gmail.com)"
-    lookups = ["aapl"]
+    # user_agent = "Nunya Business (nunyabusiness@gmail.com)"
+    # lookups = ["aapl"]
 
-    submissions = get_submissions(lookups=lookups, user_agent=user_agent, recent=False)
-    print(submissions["aapl"])
+    # submissions = get_submissions(lookups=lookups, user_agent=user_agent, recent=False)
+    # print(submissions["aapl"])
+
+    print(_combine_dicts({"A": [1, 2, 3], "B": [4, 5, 6]}, {"A": [7, 8], "B": [0, 1, 2]}))
 
     # concepts = get_company_concepts(lookups=lookups,
     #                                 user_agent=user_agent,
