3d608712847225ffeea78cac1a81a1f2ab3a5675
Jack Moody <jacklaytonmoody@gmail.com>
2022-08-12 21:51:49 -0400

    TEST: Add tests for REST API

---
+++ secedgar/tests/core/test_rest.py
@@ -0,0 +1,68 @@
+import pytest
+from secedgar.core.rest import (get_company_concepts, get_company_facts,
+                                get_submissions, get_xbrl_frames)
+
+
+class TestRest:
+    @pytest.mark.smoke
+    def test_get_submissions(self, mock_user_agent):
+        submissions = get_submissions(lookups=["aapl"], user_agent=mock_user_agent)
+        # Make sure Apple's CIK shows up properly
+        assert submissions["aapl"]["cik"] == "320193"
+
+        # Make sure there are accession numbers
+        assert submissions["aapl"]["filings"]["recent"]["accessionNumber"]
+
+    @pytest.mark.smoke
+    def test_get_company_concepts(self, mock_user_agent):
+        concept = "AccountsPayableCurrent"
+        concepts = get_company_concepts(lookups=["AAPL"],
+                                        user_agent=mock_user_agent,
+                                        concept_name=concept)
+        # Ensure CIK is correct
+        assert concepts["AAPL"]["cik"] == "320193"
+
+        # Make sure that there are results for accounts payable
+        assert concepts["AAPL"]["units"]["USD"]
+
+    @pytest.mark.smoke
+    def test_get_company_facts(self, mock_user_agent):
+        facts = get_company_facts(lookups=["aapl"], user_agent=mock_user_agent)
+
+        # Ensure CIK is correct - sometimes will give number, so cast to string
+        assert str(facts["aapl"]["cik"]) == "320193"
+
+        # Make sure there are facts
+        assert facts["aapl"]["facts"]
+
+        # Make sure that us-gaap and dei both are keys
+        assert "us-gaap" in facts["aapl"]["facts"]
+        assert "dei" in facts["aapl"]["facts"]
+
+        # Make sure we can get Revenues for Apple
+        assert facts["aapl"]["facts"]["us-gaap"]["Revenues"]["units"]["USD"]
+
+    @pytest.mark.smoke
+    @pytest.mark.parametrize(
+        "concept",
+        [
+            "Revenues"
+        ]
+    )
+    def test_get_xbrl_frames(self, mock_user_agent, concept):
+        frames = get_xbrl_frames(user_agent=mock_user_agent,
+                                 concept_name=concept,
+                                 year=2020,
+                                 quarter=1,
+                                 instantaneous=False)
+        # Check to make sure we got the right frame
+        assert frames["tag"] == concept
+
+        # Make sure there is data
+        assert frames["data"]
+
+        # Data should have accn and cik as keys
+        assert "accn" in frames["data"][0]
+        assert "cik" in frames["data"][0]
+
+        frames
