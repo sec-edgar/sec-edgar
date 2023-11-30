41c3e329399aaa95b6fe38a1c5c20c9e77a24da2
Clint Moyer <contact@clintmoyer.com>
2023-11-30 11:39:15 -0800

    Pass user agent to cik lookup
    The CIK lookup (central index key) uses the EDGAR interface to get
    tickers and company names. The lookup is separate from the core REST
    interface which is where most of the EDGAR calls are made. The CIK
    lookup also requires a valid user agent being subject to the same filter
    rules as the others.
    
    SEC blocks requests where user agent contains the string
    "python-request". There may be other filters present but not documented.

--- secedgar/cik_lookup.py
+++ secedgar/cik_lookup.py
@@ -8,7 +8,7 @@ from secedgar.exceptions import CIKError, EDGARQueryError
 
 
 @functools.lru_cache()
-def get_cik_map():
+def get_cik_map(user_agent=None):
     """Get dictionary of tickers and company names to CIK numbers.
 
     Uses ``functools.lru_cache`` to cache response if used in later calls.
@@ -29,7 +29,11 @@ def get_cik_map():
 
     .. versionadded:: 0.1.6
     """
-    response = requests.get("https://www.sec.gov/files/company_tickers.json")
+    if user_agent:
+        headers = {'user-agent': user_agent}
+    else:
+        headers = {}
+    response = requests.get("https://www.sec.gov/files/company_tickers.json", headers=headers)
     json_response = response.json()
     return {key: {v[key].upper(): str(v["cik_str"]) for v in json_response.values()
                   if v[key] is not None}
@@ -201,7 +205,7 @@ class CIKLookup:
         ciks = {}
         to_lookup = set(self.lookups)
 
-        cik_map = get_cik_map()
+        cik_map = get_cik_map(self.client.user_agent)
 
         # all keys upper case
         ticker_map = cik_map["ticker"]
